a4622b244480830308401e029b771371eba88922
YanagiEiichi <admin@web-tinker.com>
2015-03-09 14:50:58 +0800

    Add performance.timing

--- ubt.js
+++ ubt.js
@@ -97,5 +97,37 @@ void function() {
     }
   });
 
+  // 记录 timing
+  void function() {
+    if(!window.addEventListener || !window.performance || !window.performance.timing) return;
+    addEventListener('load', function() {
+      setTimeout(function(){
+        var timing = performance.timing;
+        var keys = [
+          'fetchStart',
+          'connectEnd',
+          'connectStart',
+          'domComplete',
+          'domContentLoadedEventEnd',
+          'domContentLoadedEventStart',
+          'domInteractive',
+          'domLoading',
+          'domainLookupEnd',
+          'domainLookupStart',
+          'loadEventEnd',
+          'loadEventStart',
+          'requestStart',
+          'responseEnd',
+          'responseStart'
+        ];
+        var data = {};
+        for(var i = 0; i < keys.length; i++) {
+          data[keys[i]] = timing[keys[i]] - timing.navigationStart;
+        }
+        UBT.send('TIMING', data);
+      });
+    });
+  }();
+
 }();
 
