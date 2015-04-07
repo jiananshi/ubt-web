2c68bed2a2de7616aca8cbc8916489d03f6e187f
YanagiEiichi <admin@web-tinker.com>
2015-04-07 13:50:59 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php
-header('Content-Type: text/plain');
+if(array_key_exists('HTTP_REFERER', $_SERVER)) {
+  header('Content-Type: image/gif');
+} else {
+  header('Content-Type: text/plain');
+}
 
 $data = urldecode($_SERVER['QUERY_STRING']);
 $data = json_decode($data, true);
