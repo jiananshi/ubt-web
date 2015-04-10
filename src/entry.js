849bdfe23d3a4065427f87995a34e9a62e9951a8
YanagiEiichi <admin@web-tinker.com>
2015-04-10 11:47:18 +0800

    Add Angular and AMD support

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,19 @@
 import { UBT } from 'src/kernel';
-import 'src/timing';
 
+import 'src/timing';
 import 'src/event/click';
 import 'src/event/change';
 import 'src/event/error';
 
-window.UBT = UBT;
+void function() {
+  switch(true) {
+    case typeof angular === 'object' && typeof angular.module === 'function':
+      return angular.module('UBT', []).factory('UBT', function() { return UBT; });
+    case typeof define === 'function' && !!define.amd:
+      return define(function() { return UBT; });
+    default:
+      [eval][0]('var UBT');
+      window.UBT = UBT;
+  }
+}();
 
