7792b5b5f1889873aea682fcef1b593b6fd2076c
YanagiEiichi <admin@web-tinker.com>
2015-05-01 02:04:52 +0800

    Remove AMD support

--- src/entry.js
+++ src/entry.js
@@ -9,8 +9,6 @@ void function() {
   switch(true) {
     case typeof angular === 'object' && typeof angular.module === 'function':
       return angular.module('UBT', []).factory('UBT', function() { return UBT; });
-    case typeof define === 'function' && !!define.amd:
-      return define(function() { return UBT; });
     default:
       [eval][0]('var UBT');
       window.UBT = UBT;
