2b67c3aa7919928983841ebd2af6cd80d105fcc3
YanagiEiichi <admin@web-tinker.com>
2015-08-17 10:46:48 +0800

    Fix rootScope error

--- ubt-angular/module.js
+++ ubt-angular/module.js
@@ -1,7 +1,11 @@
 var angular = require('./angular');
-module.exports = angular.module('UBT', []).factory('UBT', function() {
+module.exports = angular.module('UBT', []).factory('UBT', ['$rootScope', function($rootScope) {
   return require('../lib/entry.js').bindData({
-    user_id: function() { return angular.element(document).scope().user.id; },
-    geohash: function() { return localStorage.getItem('GEOHASH') || ''; }
+    user_id: function() {
+      return $rootScope.user && $rootScope.user.id;
+    },
+    geohash: function() {
+      return localStorage.getItem('GEOHASH') || '';
+    }
   });
-});
+}]);
