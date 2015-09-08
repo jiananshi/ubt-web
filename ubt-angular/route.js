0c72cdaf397b556784b5c6241bea1a1510eca6d4
YanagiEiichi <admin@web-tinker.com>
2015-09-08 13:54:38 +0800

    Add ui-route event

--- ubt-angular/route.js
+++ ubt-angular/route.js
@@ -2,12 +2,14 @@ require('./module').run(['UBT', '$rootScope', function(UBT, $rootScope) {
   // 路由切换，产生新的 pvhash
   var referrer = document.referrer;
   var html = document.documentElement;
-  $rootScope.$on('$routeChangeSuccess', function() {
+  var routeChange = function() {
     UBT.send('PV', {
       resolution: Math.max(html.clientWidth, window.innerWidth || 0) + 'x' + Math.max(html.clientHeight, window.innerHeight || 0),
       location: location.href,
       referrer: referrer
     });
     referrer = location.href;
-  });
+  };
+  $rootScope.$on('$routeChangeSuccess', routeChange);
+  $rootScope.$on('$stateChangeSuccess', routeChange);
 }]);
